b6543a8737c2a9defb84182c275d72d3c6681900
Finix-Projetcs <jarbull87@gmail.com>
2021-10-15 20:14:17 +0700

    Create kernel.sh

---
+++ kernel.sh
@@ -0,0 +1,5 @@
+git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/Finix_kernel-4.9-rosy -b eleven && cd Finix*
+git config --global user.email jarbull87@gmail.com
+git config --global user.name AnGgIt88
+git reset --hard 994c1510b79c210704aad5d09e6e1ad0da6b5b7a
+git push -f origin HEAD
