8e5b374be801fcbf050b8d26d52020de4365f239
Finix-Projetcs <jarbull87@gmail.com>
2021-10-18 10:59:41 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,4 +1,3 @@
 git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/Finix_kernel-4.9-rosy -b eleven && cd Finix*
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
-git remote add mido https://github.com/zeelog/android_kernel_xiaomi_mido && git fetch mido lineage-18.1
