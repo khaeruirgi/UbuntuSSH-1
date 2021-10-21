2f997dff3a46af2828ea11a91760b1dd56953ef9
Finix-Projetcs <jarbull87@gmail.com>
2021-10-21 09:04:17 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,5 @@
 git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/xd_vendor_xiaomi_rosy -b eleven && cd xd_vendor_xiaomi_rosy
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
+git revert 4f1b3df6218f0774e07dd87223a7f0020da152e0
+git push
