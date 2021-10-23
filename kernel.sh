06ec79c0b743ff5f64a14fcf64efec864f65a92f
Finix-Projetcs <jarbull87@gmail.com>
2021-10-23 16:34:40 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,5 +1,3 @@
-git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/xd_vendor_xiaomi_rosy -b eleven && cd xd_vendor_xiaomi_rosy
+git clone https://AnGgIt88:$GH_TOKEN@github.com/AnGgIt88/Finix_kernel-4.9-rosy -b eleven && cd xd_vendor_xiaomi_rosy
 git config --global user.email jarbull87@gmail.com
 git config --global user.name AnGgIt88
-git revert 4f1b3df6218f0774e07dd87223a7f0020da152e0
-git push
