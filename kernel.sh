7bb91b2779e418b02db076a15713c43084a83c6c
khaeruirgi <83441763+khaeruirgi@users.noreply.github.com>
2021-11-18 08:32:48 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,3 @@
-git clone https://khaeruirgi:$GH_TOKEN@github.com/khaeruirgi/kucingkernel_mt6768 sampah && cd sampah
+git clone https://khaeruirgi:$GH_TOKEN@github.com/khaeruirgi/kucingkernel_g8x sampah && cd sampah
 git config --global user.email khaeruirgi24@gmail.com
 git config --global user.name khaeruirgi
