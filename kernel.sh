e9a9684f1dc29c79b5f939a1f4930fc18f00bb31
Khaeru Irgi <83441763+khaeruirgi@users.noreply.github.com>
2023-08-13 05:31:58 +0700

    Update kernel.sh

--- kernel.sh
+++ kernel.sh
@@ -1,3 +1,3 @@
-git clone https://khaeruirgi:$GH_TOKEN@github.com/khaeruirgi/android_vendor_crDroidOTA sampah && cd sampah
+git clone https://khaeruirgi:$GH_TOKEN@github.com/crdroidandroid/android_vendor_crDroidOTA sampah && cd sampah
 git config --global user.email khaeruirgi24@gmail.com
 git config --global user.name khaeruirgi
